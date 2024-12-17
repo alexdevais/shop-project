87a5f559322a274e5cbeb4f5d9ccdf6fbd101af4
alex <alexdevais@gmail.com>
2024-12-17 14:47:20 +0100

    add back-end project

--- front-end/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { CreateAccountComponent } from './component/create-account/create-account.component';
-import { LoginComponent } from './component/login/login.component';
-
-export const routes: Routes = [
-    { path: '', component: LoginComponent }, 
-    { path: 'login', component: LoginComponent },
-    { path: 'create-account', component: CreateAccountComponent },
-];
